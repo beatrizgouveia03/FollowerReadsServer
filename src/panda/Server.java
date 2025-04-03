119159e89a659f7b9534f1d5b1abca044de79d77
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2025-04-03 07:39:34 -0300

    ADDED abstractions of the server class

--- src/panda/Server.java
+++ src/panda/Server.java
@@ -1,5 +1,12 @@
 package panda;
 
-public class Server {
-    
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public abstract class Server {
+    protected int leaderPort;
+    protected final int gatewayPort = 8080;
+    protected final int MAX_CONNECTIONS = 300;
+    protected CopyOnWriteArrayList<Integer> serverPorts;
+
+    abstract void initialize();
 }
