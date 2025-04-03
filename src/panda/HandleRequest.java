140f9610b1fa5d3e80fdee8f6a0946f204c8b5da
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2025-04-03 07:40:42 -0300

    INITIALIZED runnable classes

--- src/panda/HandleRequest.java
+++ src/panda/HandleRequest.java
@@ -1,5 +1,23 @@
 package panda;
 
-public class HandleRequest {
+import java.net.Socket;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class HandleRequest implements Runnable {
+    private Socket socket;
+    private int leaderPort;
+    private CopyOnWriteArrayList<Integer> serverPorts;
+
+    public HandleRequest(Socket socket, int leaderPort, CopyOnWriteArrayList<Integer> serverPorts){
+        this.socket = socket;
+        this.leaderPort = leaderPort;
+        this.serverPorts = serverPorts;        
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
     
 }
--- src/panda/HeartbeatMonitor.java
+++ src/panda/HeartbeatMonitor.java
@@ -1,5 +1,24 @@
 package panda;
 
-public class HeartbeatMonitor {
+public class HeartbeatMonitor implements Runnable{
+    private APIGateway gateway;
+    private final int HEARTBEAT_INTERVAL = 5000;
+
+    public HeartbeatMonitor(APIGateway gateway){
+        this.gateway = gateway;
+    }
+
+    @Override
+    public void run() {
+        while (true) { 
+            try {
+                Thread.sleep(HEARTBEAT_INTERVAL); 
+                gateway.removeServer(1); 
+            } catch (InterruptedException e) {
+                System.out.println("Monitor interrompido!");
+                break; 
+            }
+        }
+    }
     
 }
