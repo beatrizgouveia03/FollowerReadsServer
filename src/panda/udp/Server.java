823b912706360ec7f0de9499b65d2acd83104f56
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2025-04-09 15:57:10 -0300

    CREATED adapted files to udp protocol implementation

--- src/panda/udp/Server.java
+++ src/panda/udp/Server.java
@@ -2,10 +2,9 @@ package panda.udp;
 
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.io.IOException;
 
 public abstract class Server {
@@ -20,16 +19,18 @@ public abstract class Server {
         while(true){
             try{
                 System.out.println("Trying to initiate server");
-                Socket socket = new Socket("localhost", gatewayPort);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                out.println(initMessage);
-                out.flush();
-                response = in.readLine();    
-
-                in.close();
-                out.close();
+                DatagramSocket socket = new DatagramSocket();
+                byte[] message = initMessage.getBytes();
+                DatagramPacket packet = new DatagramPacket(message, message.length, InetAddress.getByName("localhost"), gatewayPort);
+                socket.send(packet);
                 socket.close();
+
+                DatagramSocket receiveSocket = new DatagramSocket(gatewayPort);
+                byte[] receiveMsg = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveMsg, receiveMsg.length);
+                receiveSocket.receive(receivePacket);
+                response = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                receiveSocket.close();
             } catch (IOException e) {
 				e.printStackTrace();
             } finally{
